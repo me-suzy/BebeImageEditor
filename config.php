6ad44f4b8cbc784172247a611441ee977770662c
Doar EU <me.suzana@gmail.com>
2026-03-16 10:59:26 +0200

    Add config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Configurare Bebe Image Editor
+ *
+ * Variantă 1 – XAMPP (recomandat):
+ *   Copiază folderul în C:\xampp\htdocs\ și deschide http://localhost/Bebe%20Image%20Editor/
+ *   Pornește doar Apache din XAMPP Control Panel.
+ *
+ * Variantă 2 – PHP built-in server:
+ *   Rulează start-server.bat sau: php -S 127.0.0.1:8765 -t .
+ *   Deschide http://127.0.0.1:8765
+ */
+return [
+    'port'     => 8765,
+    'host'     => '127.0.0.1',
+    'base_url' => 'http://127.0.0.1:8765',
+    'xampp_url' => 'http://localhost/Bebe%20Image%20Editor/',
+];
